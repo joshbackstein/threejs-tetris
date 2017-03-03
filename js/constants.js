10921cca2f5c5f6675b68cdb672180211424dd90
joshbackstein <josh.backstein@gmail.com>
2017-03-03 15:43:10 -0700

    Added board and block drawing.

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Global constants.
+const ADD_FLOOR = true;
+const ADD_GRID_HELPER = true;
+const ADD_AXIS_HELPER = !true;
+const CUBE_SIZE = 40;
+const BOARD_SIZE = 10;
+const BOARD_HEIGHT = 20;
+const CAMERA_Y = 300;
+const CAMERA_X = 600;
+const CAMERA_Z = 600;
